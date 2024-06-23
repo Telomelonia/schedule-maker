5f65e432fe4c7ffb937d1380890181668da9b889
telomelonia <shuklas2002@gmail.com>
2024-06-23 18:46:30 +0530

    planner feature installed

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
 
+    from .routes.planner import planner_bp
+    app.register_blueprint(planner_bp)
+
     from .routes.scheduler import scheduler_bp
     app.register_blueprint(scheduler_bp, url_prefix='/scheduler')
 
